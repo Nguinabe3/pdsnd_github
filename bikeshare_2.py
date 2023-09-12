895cfadc39f8b0b09b8f0efa977f2819cdc3fd4a
NGUINABE JOSUE <josue.nguinabe@aims-cameroon.org>
2023-09-12 13:53:44 +0100

    Remove unusable dictionary in the file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -6,10 +6,6 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
-
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
